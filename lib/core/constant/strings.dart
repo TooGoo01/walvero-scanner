502430fb717b0f2108e6064b12243325006a844a
Elmir Gasimzada <elmir.gasimzada@gmail.com>
2026-04-13 10:29:34 +0400

    role adding

--- lib/core/constant/strings.dart
+++ lib/core/constant/strings.dart
@@ -3,8 +3,8 @@ const String appTitle = 'Walvero Partner';
 
 // Networking and APIs
 //const String baseUrl = 'https://e-commerce-mock-api-d8zv.onrender.com';
- const String baseUrl = 'https://appapi.walvero.com';  
-// const String baseUrl = 'https://dev.walvero.com';  
+//const String baseUrl = 'https://appapi.walvero.com';  
+ const String baseUrl = 'https://dev.walvero.com';  
 
 const String defaultApiKey = '';
 const String defaultSources = '';
